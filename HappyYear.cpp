cd23145f0381e79af6fa165dbb59ad5c9334e6c7
ALBaraa-techie <alasdjymnj581@gmail.com>
2026-02-10 10:29:03 +0200

    Create HappyYear.cpp

---
+++ HappyYear.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+bool isHappy(int year){
+   if(year%4==0){
+      return true;
+   }
+   else{
+      return false;
+   }
+}
+//----------------------------------------
+int main(){
+  int Year;
+  cout<<"Enter the year:"<<endl;
+  cin>>Year;
+
+  if(isHappy(Year)){
+   cout<<"Happy Year";
+  }
+  else{
+   cout<<"Normal Year";
+  }
+     
+   return 0;
+}
